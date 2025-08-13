8d2e5cc72386695b3e8d9011d8393f62cd91be5e
Naoyuki Yoshinori <1448309+naoyoshinori@users.noreply.github.com>
2025-08-13 02:39:56 +0000

    refactor(build): remove legacy build and release scripts
    Deletes the shell scripts previously used for building and releasing Docker images (build.sh, release.sh, etc.) and their associated configuration files.
    
    This change streamlines the workflow, relying on the newer processes defined in the GitHub Actions workflow.

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-variants=("typescript-node" "javascript-node" "node")
-
-for variant in "${variants[@]}"; do
-  ./build.sh "$variant"
-done
--- release_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-variants=("typescript-node" "javascript-node" "node")
-
-for variant in "${variants[@]}"; do
-  ./release.sh "$variant"
-done
